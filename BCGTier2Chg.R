d2ec43f2cf37debb2c783cbab170bdc8e93936cb
marybecker <mary.becker@ct.gov>
2020-02-26 12:01:51 -0500

    update bug mets

--- BCGTier2Chg.R
+++ BCGTier2Chg.R
@@ -1,23 +1,27 @@
-setwd("P:/Projects/GitHub_Prj/BioVariability")
+setwd("/home/mkozlak/Documents/Projects/GitHub/BioVariability")
 
 library(ggplot2)
 library(vegan)
 library(rgdal)
 library(stringr)
 
-BCG<-read.csv("data/bugBCG2_MutliYr_02112020.csv",header=TRUE)
+tier<-2
+attr<-2
+
+BCG<-read.csv(paste0("data/bugBCG",tier,"_MutliYr_02112020.csv"),header=TRUE)
 BCG$YrGrp<-ifelse(BCG$SampYr< 2000,1,ifelse(BCG$SampYr< 2010,3,2))
-taxa<-read.csv("data/taxaData_Tier2.csv",header=TRUE)
+taxa<-read.csv(paste0("data/taxaData_Tier",tier,".csv"),header=TRUE)
+
 
 taxa_agg<-taxa[which(taxa$BCG_Attribute==2|taxa$BCG_Attribute==3),]
-#taxa_agg<-taxa[which(taxa$BCG_Attribute>=4),]
+#taxa_agg<-taxa[which(taxa$BCG_Attribute>=5),]
 taxa_agg<-aggregate(taxa_agg$ABUNDANCE,by=as.list(taxa_agg[,c("STA_SEQ","SampYr")]),FUN=sum)
 BCG<-merge(BCG,taxa_agg,by=c("STA_SEQ","SampYr"),all.x=TRUE)
 BCG$x[is.na(BCG$x)]<-0
 BCG$RelAbund<-BCG$x/BCG$SumOfABUNDANCE
 
 ###################################################################################################
-TPColors=c("#e66101","#fdb863","#636363","#49006a")
+TPColors=c("#a6611a","#404040","#018571","#49006a")
 
 # BCGBP<- ggplot(BCG,aes(as.factor(YrGrp),RelAbund))+
 #   geom_boxplot(fill=TPColors[1:3])+
@@ -66,33 +70,66 @@ BCGYrDiff$YrDiff<-BCGYrDiff$SampYr1-BCGYrDiff$SampYr2
 BCGYrDiff$BCGDiff<-BCGYrDiff$BCGYr1-BCGYrDiff$BCGYr2
 BCGYrDiff$ABSBugDiff<-abs(BCGYrDiff$BugYr1-BCGYrDiff$BugYr2)
 
-summary(BCGYrDiff[BCGYrDiff$YrDiff<=5,11])
-####NEEDS WORK**************################################################
+#############SUMMARY STATS############################################################3
+cln<-8
+
+#Summary stats of differences in samples taken within 5 Years apart
+nbase<-dim(BCGYrDiff[BCGYrDiff$YrDiff<=5,])[1]#n combinations of samples within 5 Years apart
+statbase<-summary(BCGYrDiff[BCGYrDiff$YrDiff<=5,cln])
+quantbase<-quantile(BCGYrDiff[BCGYrDiff$YrDiff<=5,cln],c(0.05,0.95))
+stdevbase<-sd(BCGYrDiff[BCGYrDiff$YrDiff<=5,cln])
+
+n20<-dim(BCGYrDiff[BCGYrDiff$YrDiff>=20,])[1]#n combinations of samples 20 Years apart
+stat20<-summary(BCGYrDiff[BCGYrDiff$YrDiff>=20,cln])
+quant20<-quantile(BCGYrDiff[BCGYrDiff$YrDiff>=20,cln],c(0.05,0.95))
+stdev20<-sd(BCGYrDiff[BCGYrDiff$YrDiff>=20,cln])
+
+n10<-dim(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]#n combinations of samples 10 Years apart
+stat10<-summary(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,cln])
+quant10<-quantile(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,cln],c(0.05,0.95))
+stdev10<-sd(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,cln])
+
+n5<-dim(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]#n combinations of samples 5-10 Years apart
+stat5<-summary(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,cln])
+quant5<-quantile(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,cln],c(0.05,0.95))
+stdev5<-sd(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,cln])
+
+BCGStat <-as.data.frame(rbind(statbase,stat20,stat10,stat5))
+BCGStat$TimeP<-c("5 Yrs","20-30 Yrs","10-20 Yrs","5-10 Yrs")
+BCGStat$N<-c(nbase,n20,n10,n5)
+quant<-rbind(quantbase,quant20,quant10,quant5)
+stdev<-rbind(stdevbase,stdev20,stdev10,stdev5)
+BCGStat<-cbind(BCGStat,quant,stdev)
+
+write.csv(BCGStat,paste0("bugPlots/BCGStatDiff",tier,"Attr",attr,".csv"),row.names=FALSE)
+
+
+####BCGAttrMets##################################################################################################
 DescBase<- dim(BCGYrDiff[BCGYrDiff$BugDiff<(0)&BCGYrDiff$YrDiff<=5,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff<=5,])[1]
 MedDescBase<- dim(BCGYrDiff[BCGYrDiff$BugDiff<(-0.1)&BCGYrDiff$YrDiff<=5,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff<=5,])[1]
 BCGDBase<- dim(BCGYrDiff[BCGYrDiff$BCGDiff<(0)&BCGYrDiff$YrDiff<=5,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff<=5,])[1]
-BCGIBase<- dim(BCGYrDiff[BCGYrDiff$BCGDiff>1&BCGYrDiff$YrDiff<=5,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff<=5,])[1]
+BCGIBase<- dim(BCGYrDiff[BCGYrDiff$BCGDiff>=1&BCGYrDiff$YrDiff<=5,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff<=5,])[1]
 BCGSBase<- dim(BCGYrDiff[BCGYrDiff$BCGDiff==0&BCGYrDiff$YrDiff<=5,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff<=5,])[1]
 
 
 Desc20<- dim(BCGYrDiff[BCGYrDiff$BugDiff<(0)&BCGYrDiff$YrDiff>=20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=20,])[1]
 MedDesc20<- dim(BCGYrDiff[BCGYrDiff$BugDiff<(-0.1)&BCGYrDiff$YrDiff>=20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=20,])[1]
 BCGD20<- dim(BCGYrDiff[BCGYrDiff$BCGDiff<(0)&BCGYrDiff$YrDiff>=20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=20,])[1]
-BCGI20<- dim(BCGYrDiff[BCGYrDiff$BCGDiff>1&BCGYrDiff$YrDiff>=20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=20,])[1]
+BCGI20<- dim(BCGYrDiff[BCGYrDiff$BCGDiff>=1&BCGYrDiff$YrDiff>=20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=20,])[1]
 BCGS20<- dim(BCGYrDiff[BCGYrDiff$BCGDiff==0&BCGYrDiff$YrDiff>=20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=20,])[1]
 
 
 Desc10<- dim(BCGYrDiff[BCGYrDiff$BugDiff<(0)&BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]
 MedDesc10<- dim(BCGYrDiff[BCGYrDiff$BugDiff<(-0.1)&BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]
 BCGD10<- dim(BCGYrDiff[BCGYrDiff$BCGDiff<0&BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]
-BCGI10<- dim(BCGYrDiff[BCGYrDiff$BCGDiff>1&BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]
+BCGI10<- dim(BCGYrDiff[BCGYrDiff$BCGDiff>=1&BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]
 BCGS10<- dim(BCGYrDiff[BCGYrDiff$BCGDiff==0&BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,])[1]
 
 
 Desc5<- dim(BCGYrDiff[BCGYrDiff$BugDiff<(0)&BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]
 MedDesc5<- dim(BCGYrDiff[BCGYrDiff$BugDiff<(-0.1)&BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]
 BCGD5<- dim(BCGYrDiff[BCGYrDiff$BCGDiff<0&BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]
-BCGI5<- dim(BCGYrDiff[BCGYrDiff$BCGDiff>1&BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]
+BCGI5<- dim(BCGYrDiff[BCGYrDiff$BCGDiff>=1&BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]
 BCGS5<- dim(BCGYrDiff[BCGYrDiff$BCGDiff==0&BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,])[1]
 
 
@@ -106,54 +143,67 @@ BCGMet <- data.frame(TimeP=factor(c("20-30 Yrs","10-20 Yrs","5-10 Yrs","5 Yrs"),
 names(TPColors)<-BCGMet$TimeP
 
 DescP<-ggplot(BCGMet,aes(TimeP,Desc))+
-  geom_col(aes(fill=TimeP,alpha=0.8))+
-  labs(y="Percent Decreasing")+
-  #ylim(0,1)+
+  geom_col(aes(fill=TimeP,alpha=1))+
+  labs(y="Proportion Decreasing")+
+  geom_text(aes(label=round(Desc,2)),position=position_dodge(width=0.9),vjust=-0.25)+
+  ylim(0,1)+
   scale_fill_manual(values=TPColors)+
-  theme(axis.title.x=element_blank(),legend.position="none")
+  theme(axis.title.x=element_blank(),legend.position="none",
+        panel.background = element_rect(fill = "white", colour = "grey"))
 
-MedDescP<- ggplot(BCGMet,aes(TimeP,MedDesc))+
-  geom_col(aes(fill=TimeP,alpha=0.8))+
-  labs(y="Percent Decreasing Greater Than Median")+
-  #ylim(0,1)+
-  scale_fill_manual(values=TPColors)+
-  theme(axis.title.x=element_blank(),legend.position="none")
+ggsave(plot=DescP,paste0("bugPlots/PctTaxaAttChg",tier,"Att",attr,".jpg"),width=5,height=5,units="in")
 
-BCGDP<-ggplot(BCGMet,aes(TimeP,BCGI))+
-  geom_col(aes(fill=TimeP,alpha=0.8))+
-  labs(y="Percent Change of BCG Increase")+
-  #ylim(0,1)+
-  scale_fill_manual(values=TPColors)+
-  theme(axis.title.x=element_blank(),legend.position="none")
+# MedDescP<- ggplot(BCGMet,aes(TimeP,MedDesc))+
+#   geom_col(aes(fill=TimeP,alpha=0.8))+
+#   labs(y="Percent Decreasing Greater Than Median")+
+#   #ylim(0,1)+
+#   scale_fill_manual(values=TPColors)+
+#   theme(axis.title.x=element_blank(),legend.position="none")
+# 
+# BCGDP<-ggplot(BCGMet,aes(TimeP,BCGI))+
+#   geom_col(aes(fill=TimeP,alpha=0.8))+
+#   labs(y="Percent Change of BCG Increase")+
+#   #ylim(0,1)+
+#   scale_fill_manual(values=TPColors)+
+#   theme(axis.title.x=element_blank(),legend.position="none")
 
 
 DiffHistBase<-  ggplot(BCGYrDiff[BCGYrDiff$YrDiff<=5,],aes(x=BugDiff,y=(..count..)/sum(..count..)))+
-  geom_histogram(binwidth=0.1,fill=TPColors[4],alpha=0.7)+
-  labs(x="Difference in Taxa Rel Abundance Between Two Years",y="Percent of Samples")+
-  xlim(-0.6,0.6)
+  geom_histogram(binwidth=0.05,fill=TPColors[4],alpha=0.7)+
+  labs(x="Difference in Taxa Rel Abundance Between Two Years",y="Proportion of Samples")+
+  xlim(-0.6,0.6)+
+  theme(panel.background = element_rect(fill = "white", colour = "grey"))
 
 DiffHist20<-  ggplot()+
   geom_histogram(data=BCGYrDiff[BCGYrDiff$YrDiff>=20,],aes(x=BugDiff,y=(..count..)/sum(..count..)),binwidth=0.05,fill=TPColors[1],alpha=0.9)+
   geom_histogram(data=BCGYrDiff[BCGYrDiff$YrDiff<=5,],aes(x=BugDiff,y=(..count..)/sum(..count..)),binwidth=0.05,fill=TPColors[4],alpha=0.4)+
-  labs(x="Difference in Taxa Rel Abundance Between Two Samples",y="Percent of Samples")+
-  xlim(-0.6,0.6)
+  labs(x="Difference in Taxa Rel Abundance Between Two Samples",y="Proportion of Samples")+
+  xlim(-0.6,0.6)+
+  theme(panel.background = element_rect(fill = "white", colour = "grey"))
 
 DiffHist10<-  ggplot()+
   geom_histogram(data=BCGYrDiff[BCGYrDiff$YrDiff>=10&BCGYrDiff$YrDiff<20,],aes(x=BugDiff,y=(..count..)/sum(..count..)),binwidth=0.05,fill=TPColors[2],alpha=0.9)+
   geom_histogram(data=BCGYrDiff[BCGYrDiff$YrDiff<=5,],aes(x=BugDiff,y=(..count..)/sum(..count..)),binwidth=0.05,fill=TPColors[4],alpha=0.4)+
-  labs(x="Difference in Taxa Rel Abundance Between Two Samples",y="Percent of Samples")+
-  xlim(-0.6,0.6)
+  labs(x="Difference in Taxa Rel Abundance Between Two Samples",y="Proportion of Samples")+
+  xlim(-0.6,0.6)+
+  theme(panel.background = element_rect(fill = "white", colour = "grey"))
 
 DiffHist5<- ggplot()+
-  geom_histogram(data=BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,],aes(x=BugDiff,y=(..count..)/sum(..count..)),binwidth=0.1,fill=TPColors[3],alpha=0.9)+
-  geom_histogram(data=BCGYrDiff[BCGYrDiff$YrDiff<=5,],aes(x=BugDiff,y=(..count..)/sum(..count..)),binwidth=0.1,fill=TPColors[4],alpha=0.4)+
-  labs(x="Difference in Taxa Rel Abundance Between Two Samples",y="Percent of Samples")+
-  xlim(-0.6,0.6)
+  geom_histogram(data=BCGYrDiff[BCGYrDiff$YrDiff>5&BCGYrDiff$YrDiff<10,],aes(x=BugDiff,y=(..count..)/sum(..count..)),binwidth=0.05,fill=TPColors[3],alpha=0.9)+
+  geom_histogram(data=BCGYrDiff[BCGYrDiff$YrDiff<=5,],aes(x=BugDiff,y=(..count..)/sum(..count..)),binwidth=0.05,fill=TPColors[4],alpha=0.4)+
+  labs(x="Difference in Taxa Rel Abundance Between Two Samples",y="Proportion of Samples")+
+  xlim(-0.6,0.6)+
+  theme(panel.background = element_rect(fill = "white", colour = "grey"))
 
-#Pct of samples that do not change cold/not cold category between combinations of samples
+##histogram plots
+ggsave(plot=DiffHistBase,paste0("bugPlots/DiffHistBaseTier",tier,"Att",attr,".jpg"),width=5,height=5,units="in")
+ggsave(plot=DiffHist20,paste0("bugPlots/DiffHist20Tier",tier,"Att",attr,".jpg"),width=5,height=5,units="in")
+ggsave(plot=DiffHist10,paste0("bugPlots/DiffHist10Tier",tier,"Att",attr,".jpg"),width=5,height=5,units="in")
+ggsave(plot=DiffHist5,paste0("bugPlots/DiffHist5Tier",tier,"Att",attr,".jpg"),width=5,height=5,units="in")
+
+#Pct of samples that do not change between combinations of samples
 dim(BCGYrDiff[BCGYrDiff$BugDiff==0&BCGYrDiff$YrDiff<=5|BCGYrDiff$BugDiff==2&BCGYrDiff$YrDiff<=5,])[1]/dim(BCGYrDiff[BCGYrDiff$YrDiff<=5,])[1]
 dim(BCGYrDiff[BCGYrDiff$BugDiff==0|BCGYrDiff$BugDiff==2,])[1]/dim(BCGYrDiff)[1]
-
 
 ###SlopeGraph##############################################################################
 
@@ -166,28 +216,32 @@ BCGMet2 <-data.frame(TimeP=factor(c("20-30 Yrs","10-20 Yrs","5-10 Yrs","5 Yrs",
                               "Stable", "Stable", "Stable", "Stable"),
                         Met=c(BCGD20,BCGD10,BCGD5,BCGDBase,BCGI20,BCGI10,BCGI5,BCGIBase,BCGS20,BCGS10,BCGS5,BCGSBase))
 
-ggplot(BCGMet2,aes(x=TimeP,y=Met,fill=Lab))+
-  geom_bar(position="fill",stat="identity")+
-  scale_y_continuous(labels=scales::percent_format())+
-  scale_fill_manual(values=c("#a6611a","#404040","#018571"))+
-  coord_flip()+
-  annotate("text",x=c(4.5,4.5,4.5),y=c(0.1,0.77,0.93),label=c("Stable","Increase","Decrease"))+
-  theme(legend.position = "none",panel.background = element_rect(fill = "white", colour = "white"),
-        axis.title=element_blank(),text=element_text(size=12,family="Sans"))
+# ggplot(BCGMet2,aes(x=TimeP,y=Met,fill=Lab))+
+#   geom_bar(position="fill",stat="identity")+
+#   scale_y_continuous(labels=scales::percent_format())+
+#   scale_fill_manual(values=c("#a6611a","#404040","#018571"))+
+#   coord_flip()+
+#   annotate("text",x=c(4.5,4.5,4.5),y=c(0.1,0.77,0.93),label=c("Stable","Increase","Decrease"))+
+#   theme(legend.position = "none",panel.background = element_rect(fill = "white", colour = "white"),
+#         axis.title=element_blank(),text=element_text(size=12,family="Sans"))
 
-ggplot(BCGMet2)+
-  geom_line(aes(TimeP,Met*100,group=Lab,colour=Lab),size=2)+
-  geom_point(aes(TimeP,Met*100,group=Lab),colour="white",size=6)+
-  geom_text(data=BCGMet2[BCGMet2$TimeP=="10-20 Yrs",],
-            aes(x=0,y=Met*100,label=c("Cold to Not Cold","Not Cold to Cold","Stable"),colour=Lab),hjust=0)+
-  geom_text(data=CWFishMet2[CWFishMet2$Lab=="Decrease"|
-                              CWFishMet2$Lab=="Stable",],aes(x=TimeP,y=Met*100,label=round(Met*100,0)),vjust=-1)+
-  geom_text(data=CWFishMet2[CWFishMet2$Lab=="Increase",],aes(x=TimeP,y=Met*100,label=round(Met*100,0)),vjust=1.5)+
-  scale_x_discrete(position = "top")+ 
-  scale_color_manual(values=c("#a6611a","#404040","#018571"))+
-  labs(x="Number of Years Between Samples\n")+
-  lims(y=c(0,80))+
-  theme(legend.position = "none",panel.background = element_rect(fill = "white", colour = "white"),
-        axis.title.y=element_blank(),text=element_text(size=16,family="Sans"),
-        axis.text.x=element_text(colour="black"),axis.ticks.length = unit(.5, "cm"),
-        axis.ticks.y=element_blank(),axis.text.y=element_blank())
+BCGmet2<- ggplot(BCGMet2)+
+            geom_line(aes(TimeP,Met*100,group=Lab,colour=Lab),size=2)+
+            geom_point(aes(TimeP,Met*100,group=Lab),colour="white",size=6)+
+            geom_text(data=BCGMet2[BCGMet2$TimeP=="20-30 Yrs",],
+                      aes(x=0,y=Met*100,label=c("Increase Tier","Decrease Tier","No Change"),colour=Lab),hjust=0)+
+            geom_text(data=BCGMet2[BCGMet2$Lab=="Decrease"|
+                                        BCGMet2$Lab=="Stable"|BCGMet2$Lab=="Increase",],aes(x=TimeP,y=Met*100,label=round(Met*100,0)),vjust=0)+
+            #geom_text(data=BCGMet2[BCGMet2$Lab=="Increase",],aes(x=TimeP,y=Met*100,label=round(Met*100,0)),vjust=1.5)+
+            scale_x_discrete(position = "top")+ 
+            scale_color_manual(values=c("#a6611a","#404040","#018571"))+
+            labs(x="Number of Years Between Samples\n")+
+            lims(y=c(0,80))+
+            theme(legend.position = "none",panel.background = element_rect(fill = "white", colour = "white"),
+                  axis.title.y=element_blank(),text=element_text(size=16,family="Sans"),
+                  axis.text.x=element_text(colour="black"),axis.ticks.length = unit(.5, "cm"),
+                  axis.ticks.y=element_blank(),axis.text.y=element_blank())
+
+ggsave(plot=BCGmet2,paste0("bugPlots/BCGmet2Tier",tier,".jpg"),width=8,height=5,units="in")
+
+
